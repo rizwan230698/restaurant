c87c3f4ce0a571b65d9c4e293abae18ff7cbcda0
rizwan230698 <rizwananwar230698@gmail.com>
2020-06-21 19:33:08 +0530

    minor fixes

--- src/utils/getTotal.js
+++ src/utils/getTotal.js
@@ -1,11 +1,8 @@
-export const getSubTotal = (items) =>
-  items.reduce((acc, { quantity, price }) => quantity * price + acc, 0);
+export const getSubTotal = (cartItems) =>
+  cartItems.reduce((acc, { quantity, price }) => quantity * price + acc, 0);
 
 export const getTotal = (state) => {
-  const subTotal = state.cartItems.reduce(
-    (acc, { quantity, price }) => quantity * price + acc,
-    0
-  );
+  const subTotal = getSubTotal(state.cartItems);
 
   return subTotal + (state.tip / 100) * subTotal;
 };
